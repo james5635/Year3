0a3486e421f2fee68b95ab5ed0c291d535c48d7b
james5635 <brorojame2029@gmail.com>
2025-03-22 09:54:48 +0700

    add windowform

---
+++ ISA/semester 2/WinFormsAppISAD/WinFormsAppISAD/Form1.cs
@@ -0,0 +1,10 @@
+namespace WinFormsAppISAD
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+    }
+}
---
+++ ISA/semester 2/WinFormsAppISAD/WinFormsAppISAD/Program.cs
@@ -0,0 +1,26 @@
+namespace WinFormsAppISAD
+{
+    internal static class Program
+    {
+        /// <summary>
+        ///  The main entry point for the application.
+        /// </summary>
+        [STAThread]
+        static void Main()
+        {
+            // To customize application configuration such as set high DPI settings or default font,
+            // see https://aka.ms/applicationconfiguration.
+            ApplicationConfiguration.Initialize();
+            
+            Task.Run(() =>
+            {
+                Application.Run(new frmStaffs());
+            });
+            Task.Run(() =>
+            {
+                Application.Run(new frmSuppliers());
+            });
+            while (true) { }
+        }
+    }
+}
